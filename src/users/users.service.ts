1e4b22f96a23fa06e09ab5f83c3d5aaa01064972
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-11-28 10:21:59 -0300

    mock

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'john',
+            password: 'changeme',
+          },
+          {
+            userId: 2,
+            username: 'maria',
+            password: 'guess',
+          },
+    ];
+    
+}
