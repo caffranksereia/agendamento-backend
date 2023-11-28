9500122d360a649953b818bd7e2ef7e773549142
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-11-28 18:22:39 -0300

    ensure findOnde()  not throw if  called  with valid params

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -14,7 +15,13 @@ describe('UsersService', () => {
 
   describe('findOne()', () => {
     it('should be throw if called with invalid params', async () => {
-      await expect(service.findOne({ username: '' })).rejects.toThrow();
+      await expect(service.findOne({ username: '' })).rejects.toThrow(
+        new BadRequestException(),
+      );
     });
+  });
+
+  it('should be not throw if  called  with valid params', async () => {
+    await expect(service.findOne({ username: 'maria' })).resolves.not.toThrow();
   });
 });
