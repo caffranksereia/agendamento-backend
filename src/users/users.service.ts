2233a9594af1a0d05348978b189570949562433b
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-11-28 18:59:31 -0300

    type user = any

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+export type User = any;
 @Injectable()
 export class UsersService {
-    private readonly users = [
-        {
-            userId: 1,
-            username: 'john',
-            password: 'changeme',
-          },
-          {
-            userId: 2,
-            username: 'maria',
-            password: 'guess',
-          },
-    ];
-    
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne({ username }): Promise<any> {
+    const data = await this.users.find((user) => user.username === username);
+    return data;
+  }
 }
