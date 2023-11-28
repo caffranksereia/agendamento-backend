0f94dbf0da32856b1912557eb0d4bdc5e3dc0ce4
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-11-28 18:13:17 -0300

    auth controler e service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+  async singIn(user: string, pass: string): Promise<any> {
+    const data = await this.userService.findOne(user);
+    if (data?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...res } = data;
+    return res;
+  }
+}
